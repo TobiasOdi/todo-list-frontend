567810ecee0b8520f23f747f8449790eedd33538
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2024-07-07 10:49:49 +0200

    done

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -35,4 +35,8 @@ export class LoginComponent implements OnInit {
     }
     this.isEnabled = false;
   }
+
+  goToTodos() {
+    this.router.navigateByUrl('/todos');
+  }
 }
--- src/app/services/auth-interceptor.interceptor.ts
+++ src/app/services/auth-interceptor.interceptor.ts
@@ -5,23 +5,23 @@ import { Router } from '@angular/router';
 
 export const authInterceptorInterceptor: HttpInterceptorFn = (request, next) => {
   const router = inject(Router);
-
   const token = localStorage.getItem('token');
-      if(token) {
-        request = request.clone({
-          setHeaders: { Authorization: `Token ${token}`}
-        })
-      }
 
-      return next(request).pipe(
-        catchError((err) => {
-          if(err instanceof HttpErrorResponse) {
-            if(err.status === 401) { // 401 = not authorized
-              //router.navigateByUrl('/login');
-              console.log('No Token');
-            }
+    if(token) {
+      request = request.clone({
+        setHeaders: {Authorization: `Token ${token}`}
+      })
+    }
+
+    return next(request).pipe(
+      catchError((err) => {
+        if(err instanceof HttpErrorResponse) {
+          if(err.status === 401) { // 401 = not authorized
+            //router.navigateByUrl('/login');
+            console.log('No Token');
           }
-          return throwError(() => err)
-        })
-      )
+        }
+        return throwError(() => err)
+      })
+    )
 };
